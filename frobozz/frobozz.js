4e990afcedc094669094d78e8e71c54b3ca2727c
lbrendanl <lee33@uw.edu>
2017-10-02 10:52:04 -0700

    missing js content

--- frobozz/frobozz.js
+++ frobozz/frobozz.js
@@ -1,17 +1,19 @@
 window.onload = start;
 
 // TODO make this sound nice and rhymey
-const intro = [
+var intro = [
     'You see an old, decrepit house before you.',
     'The treasure you seek is somewhere inside.',
     'But plenty of traps and obstacles stand in your way.',
     'There is something familiar about this place....'
 ]
 
-let inventory = [];
+var inventory = [];
 
-let introIndex = 0;
-let interval;
+var inventoryCommands = ['turkey', 'key', 'plate of armor', 'sword']
+
+var introIndex = 0;
+var interval;
 
 function start() {
     Terminal.init('terminal-history', 'terminal-input');
@@ -61,11 +63,11 @@ var Terminal = {
 var Game = {
     init: function (config) {
         this._config = config;
-        this._currentRoomName = this._config.startRoom;
-        this.loadRoom(this._currentRoomName);
+        this.loadRoom(this._config.startRoom);
     },
 
     loadRoom: function (roomName) {
+        this._currentRoomName = roomName;
         var room = this._config.rooms[roomName];
 
         Terminal.clearHistory();
@@ -74,6 +76,8 @@ var Game = {
         Terminal.write(this._config.messages.instructions);
         Terminal.write();
 
+        Terminal.write('0 - View Inventory.');
+        
         for (var choice in room.choices) {
             Terminal.write(choice + ' - ' + room.choices[choice].description);
         }
@@ -82,20 +86,40 @@ var Game = {
     },
 
     processCmd: function (cmd) {
+        if (cmd === '0') {
+            this.listInventory();
+            return;
+        };
+
         var room = this._config.rooms[this._currentRoomName];
 
+        for (var i in inventoryCommands) {
+            if (cmd === inventoryCommands[i]) {
+                this.attemptToUseItem(cmd);
+                return;
+            }
+        }
+
         for (var choice in room.choices) {
+            var roomInfo = room.choices[choice];
             if (cmd.trim() == choice.trim()) {
-                if (room.choices[choice].death) {
-                    Terminal.write(room.choices[choice].death);
-                    Terminal.write("Game Over......");
-                    setTimeout(() => {
-                        location.reload();
-                    }, 5000);
+                if (roomInfo.item) {
+                    if (inventory.indexOf(roomInfo.item) >= 0) {
+                        Terminal.write("You can't pick up any more of that item");
+                    } else {
+                        Terminal.write("You have found a " + roomInfo.item + " and added it to your inventory.");
+                        inventory.push(roomInfo.item);                        
+                    }
+                                        
                     return;
-                } else if (room.choices[choice] !== '') {
-                    this._currentRoomName = room.choices[choice].success;
-                    this.loadRoom(room.choices[choice]);
+                } else if (roomInfo.death) {
+                    this.gameOver(roomInfo.death);
+                    return;
+                } else if (roomInfo.tryItem) {
+                    this.attemptToUseItem(roomInfo.tryItem, roomInfo.deathWithoutItem);
+                    return;
+                } else if (roomInfo !== '') {
+                    this.loadRoom(roomInfo.success);
                     return;
                 }
 
@@ -105,6 +129,70 @@ var Game = {
         }
 
         Terminal.write(this._config.messages.inputNotUnderstood);
+    },
+
+    gameOver: function(cause) {
+        Terminal.write(cause);
+        Terminal.write("Game Over......");
+        setTimeout(() => {
+            location.reload();
+        }, 5000);
+    },
+
+    listInventory: function() {
+        Terminal.write('Your Investory: \n');
+        if (inventory.length === 0) {
+            Terminal.write('Empty.  Keep exploring!');
+        } else {
+            for (var item in inventory) {
+                Terminal.write(inventory[item]);
+            }
+            Terminal.write('\nYou can type the name of an item to attempt to use it.');            
+        }
+    },
+
+    attemptToUseItem: function(item, deathMessage) {
+        switch (item) {
+            case 'turkey':
+                if (this._currentRoomName === "Dog") {
+                    this.loadRoom("Final1");
+                    return;                    
+                }
+                break;
+            case 'key':
+                if (this._currentRoomName === "StairTop") {
+                    this.loadRoom("Hallway");
+                    return;                    
+                }
+                break;
+            case 'plate of armor':
+                if (this._currentRoomName === "Middle") {
+                    if (inventory.indexOf(item) < 0) {
+                        gameOver(deathMessage);
+                    } else { 
+                        this.loadRoom("Dog");
+                    }
+
+                    return;                    
+                }
+                break;
+            case 'sword':
+                if (this._currentRoomName === "Dog") {
+                    gameOver("You swing your sword at the Dog.  Did I mention it had two heads? You manage to injure the left head as the right head lunges at you...");
+                    return;
+                } else if (this._currentRoomName === "Outside") {
+                    Terminal.write("You best the knight in single combat, it was a glorious but short fight.");
+                    Terminal.write("With his dying breath, the knight leans in and says.....");
+                    Terminal.write("Game designers Jared and Brendan are very impressed with your adventuresome spirit.  Hope to see you at the end of the hunt!");
+                }
+                break;
+            default: 
+                if (inventory.indexOf(item) < 0) {
+                    Terminal.write("You don't have that item in your inventory.")
+                } else {
+                    Terminal.write("You can't use that here.");
+                }
+        }
     }
 };
 
@@ -120,42 +208,104 @@ var Keyboard = {
 };
 
 var gameconfig = {
-    startRoom: 'B0',
+    startRoom: 'Outside',
     messages: {
         instructions: 'What would you like to do?',
         inputNotUnderstood: 'Come again?',
         noAction: 'Nothing to do.'
     },
     rooms: {
-        'B0': {
+        'Outside': {
             description: 'You see a large knight standing blocking the entrance to the house.',
             choices: {
-                '1': { description: 'Attack him with your sword', death: 'Sorry, you do not have a sword' },
-                '2': { description: 'Make an empassioned speach about your destiny.', success: 'B1' }
+                '1': { description: 'Attack him with your sword', tryItem: 'sword', deathWithoutItem: 'Sorry, you do not have a sword.' },
+                '2': { description: 'Make an empassioned speach about your destiny.', success: 'Entry' },
+                '3': { description: 'Bribe the guard', death: 'The knight is too honorable for that...' }
             }
         },
-        'B1': {
-            description: 'In the entryway of the house, you see a giant staircase leading both up and down.  There are also doors to your left and right.',
+        // Left and right aren't an option in the flow chart,
+        // This is an early indicator that the game is slightly different.
+        'Entry': {
+            description: 'In the entry room of the house, you see a giant staircase leading both up and down.  There are also doors to your left and right.',
             choices: {
-                'Go up the stairs': { death: 'The stairs lead upwards. At the top of the stairs you fall through a trap door' },
-                'Go down the stairs': 'B2',
-                'Go into the room to the right': { death: 'some reason.' },
-                'Go into the room to the left': { death: 'some other reason.' },
+                '1': { description: 'Go up', success: 'StairTop' },
+                '2': { description: 'Go down', success: 'Basement' },
+                '3': { description: 'Go left', death: 'TODO' },
+                '4': { description: 'Go right', death: 'TODO' },
+                '5': { description: 'Leave the house', success: 'Outside' }
             }
         },
-        'B2': {
-            description: 'At the bottom of the stairs is a small basement room. To your left you can see the kitchen, to your right a closet.',
+        'Basement': {
+            description: 'You come upon the basement and enter the basement hallway. To your left you can see the kitchen, to your right a closet.',
             choices: {
-                'Head into the closet.': { death: 'Sorry, you do not have a sword' },
-                'Head into the kitchen.': { success: 'B3' }
+                '1': { description: 'Go back upstairs', success: 'Entry' },
+                '2': { description: 'Go to the kitchen', success: 'Kitchen' },
+                '3': { description: 'Go to the closet', success: 'Closet' }
             }
         },
-        'B3': {
-            description: 'You are in the kitchen.  ',
+        'Kitchen': {
+            description: 'You are in the kitchen.',
             choices: {
-                'Head into the closet.': { death: 'Sorry, you do not have a sword' },
-                'Head into the kitchen.': { success: 'B3' }
+                '1': { description: 'Grab a turkey leg for the road.', item: 'turkey' },
+                '2': { description: 'Return to the basement hallway.', success: 'Basement' }
             }
+        },
+        'Closet': {
+            description: 'You enter a dimly lit storage closet.',
+            choices: {
+                '1': { description: 'Look around the closet for item.', item: 'key' },
+                '2': { description: 'Return to the basement hallway.', success: 'Basement' }
+            }
+        },
+        'StairTop': {
+            description: 'At the top of the stairs, you encounter a locked door.',
+            choices: {
+                '1': { description: 'Go back downstairs.', success: 'Entry' },
+                '2': { description: 'Knock on the door.', death: 'A trap door opens at your feet...' }
+            }
+        },
+        'Hallway': { 
+            description: 'Success! They key worked! You enter a room on the top floor of the house.  In front of you are three unmarked doors next to eachother.',
+            choices: {
+                '1': { description: 'Go back downstairs.', success: 'Entry' },
+                '2': { description: 'Enter the door on the left.', death: 'You open the door, something something dead end and you lose.' },
+                '3': { description: 'Enter the door in the middle.', success: 'Middle' },
+                '4': { description: 'Enter the door on the right.', success: 'Right' }
+            }
+        },
+        'Middle': { 
+            description: 'You open the door in the middle.  You cannot see anything inside.',
+            choices: {
+                '1': { description: 'Turn back.', success: 'Hallway' },
+                '2': { description: 'Rush in blindly.', tryItem: 'plate of armor', deathWithoutItem: 'TODO death message.' }
+            }
+        },
+        'Right': { 
+            description: 'You open the door on the right.  It appears you have found the armory.',
+            choices: {
+                '1': { description: 'Go back to the hallway.', success: 'Hallway' },
+                '2': { description: 'Grab a sword.', item: 'sword' },
+                '3': { description: 'Grab a plate of armor.', item: 'plate of armor' },
+                '4': { description: 'Grab a shield', death: 'TODO it was a trap!' } // Too mean?
+            }
+        },
+        'Dog': { 
+            description: 'You enter a round room.  There are only three things in this room.  The door you came through, a door on the other side of the room, and a two headed dog in the middle.',
+            choices: {
+                '1': { description: 'Yikes. Go back to the hallway.', success: 'Hallway' },
+            }
+        },
+        'Final1': { 
+            description: 'You rip off a piece of the turkey and throw it on the ground.  The dog is distracted for now! You sneak past. You enter a giant room with tall ceilings.  A ray of light shines down from the ceiling in the center of the room. The light shines down upon a jewel encrusted chest.  The chest appears locked, but there is an enscription in an ancient language around the outside of the chest.',
+            choices: {
+                '1': { description: 'Read the enscription.  It is likely a spell that will unlock the chest.', death: 'The chest explodes? TODO' },
+                '2': { description: 'Kick the chest.', success: 'Final2' },
+                '3': { description: 'Go back the way you came.', success: 'Dog' }
+            }
+        },
+        'Final2': { 
+            description: 'The chest opens.  Inside, all you find is a small piece of parchment.  You unfold the parchment.  It reads SUPER_COOL_CODEWORD.  Enter it into ClueKeeper to defuse the bomb and move on in your hunt.',
+            choices: { }
         },
     }
 };
